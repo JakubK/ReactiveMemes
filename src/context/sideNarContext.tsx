60a1e75424dbf7c45c9b3b12186bd85e01c2bad2
Jakub Kuniszewski <you@example.com>
2022-10-02 13:38:14 +0200

    Fix typing to remove warnings

--- src/context/sideNarContext.tsx
+++ src/context/sideNarContext.tsx
@@ -5,13 +5,13 @@ export type SideNavContextType = {
     setIsActive: (value: boolean) => void;
 };
 
-type SideNavProps = {
-    children?: JSX.Element;
+type SideNavProviderProps = {
+    children?: JSX.Element[];
 }
 
 export const SideNavContext = React.createContext<SideNavContextType | null>(null);
 
-const SideNavProvider = (props: SideNavProps) => {
+const SideNavProvider = (props: SideNavProviderProps) => {
     const [isActive, setIsActive] = useState(false);
     return <SideNavContext.Provider value={{isActive, setIsActive}}>
         {props.children}
